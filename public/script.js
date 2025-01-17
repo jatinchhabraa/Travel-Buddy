2f3b59968f1479715443d65ce6f9b9241c5d8076
jatinchhabraa <jchhabra_be22@thapar.edu>
2025-01-17 18:19:58 +0530

    intial commit: setting up landing page

---
+++ public/script.js
@@ -0,0 +1,7 @@
+// script.js
+document
+  .getElementById("start-planning")
+  .addEventListener("click", function () {
+    // Redirect to the trip planner page
+    window.location.href = "trip-planner.html";
+  });
